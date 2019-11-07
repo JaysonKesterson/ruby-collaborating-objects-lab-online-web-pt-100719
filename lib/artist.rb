9c1daeebac65f8d103da4420eed29c14785213a0
Jayson Kesterson <jayson.kesterson24@gmail.com>
2019-11-07 15:54:15 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,38 +1,38 @@
-class Artist
+# class Artist
   
-  attr_accessor :name, :songs
-  @@all = []
+#   attr_accessor :name, :songs
+#   @@all = []
   
-  def initialize(name)
-    @name = name
-    @songs = []
-    @@all << self
-  end
+#   def initialize(name)
+#     @name = name
+#     @songs = []
+#     @@all << self
+#   end
   
-  def self.all
-    @@all.uniq
-  end 
+#   def self.all
+#     @@all.uniq
+#   end 
   
-  def songs 
-    @songs
-  end
+#   def songs 
+#     @songs
+#   end
   
-  def add_song(song)
-    self.songs << song
-  end
+#   def add_song(song)
+#     self.songs << song
+#   end
   
-  def save 
-    @@all << self
-    self
-  end 
+#   def save 
+#     @@all << self
+#     self
+#   end 
   
-  def self.find_or_create_by_name(name)
-    self.all.find{|artist| artist.name == name} || self.new(name).save
-  end
+#   def self.find_or_create_by_name(name)
+#     self.all.find{|artist| artist.name == name} || self.new(name).save
+#   end
   
-  def print_songs
-    @songs.each do |song|
-      puts song.name
-    end
-  end
-end
+#   def print_songs
+#     @songs.each do |song|
+#       puts song.name
+#     end
+#   end
+# end
